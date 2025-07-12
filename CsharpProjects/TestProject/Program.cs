028ba06aaaeee267910a9239058fb2c8eaeba313
Ishaan Mittal <123089344+IshaanMittal07@users.noreply.github.com>
2025-07-11 23:54:16 -0400

    Update Program.cs

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Yo, World!");
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
