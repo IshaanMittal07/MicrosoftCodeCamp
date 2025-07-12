d92330c3f420a62d8f480a8de83b51f9cebb6e63
Ishaan Mittal <123089344+MathGamer2012@users.noreply.github.com>
2025-07-12 00:10:29 -0400

    game

--- CsharpProjects/IfStatment/Program.cs
+++ CsharpProjects/IfStatment/Program.cs
@@ -1,9 +1,9 @@
-﻿Random dice = new Random();
+﻿if (total > 14)
+{
+    Console.WriteLine("You win!");
+}
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
+}
