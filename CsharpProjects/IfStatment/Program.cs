283c9685f35ff5e6d27c651ece09c8b6d089d001
Ishaan Mittal <123089344+MathGamer2012@users.noreply.github.com>
2025-07-12 00:12:32 -0400

    .contains

--- CsharpProjects/IfStatment/Program.cs
+++ CsharpProjects/IfStatment/Program.cs
@@ -1,20 +1,8 @@
-﻿Random dice = new Random();
+﻿string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-
-if (total > 14)
+if (message.Contains("fox"))
 {
-    Console.WriteLine("You win!");
-}
-
-if (total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("What does the fox say?");
 }
