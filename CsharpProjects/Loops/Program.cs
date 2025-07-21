fff4074610e7b77bfc3a0ce6fc833538f3184356
Ishaan Mittal <123089344+MathGamer2012@users.noreply.github.com>
2025-07-21 15:56:10 -0400

    challenge done

--- CsharpProjects/Loops/Program.cs
+++ CsharpProjects/Loops/Program.cs
@@ -1,10 +1,9 @@
-﻿string[] fraudulentOrderIDs = new string[3];
+﻿string[] ID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-// fraudulentOrderIDs[3] = "D000";
-
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+foreach (string name in ID)
+{
+    if (name.StartsWith("B"))
+    {
+        Console.WriteLine(name); 
+    }
+}
--- CsharpProjects/Loops/obj/Debug/net9.0/Loops.AssemblyInfo.cs
+++ CsharpProjects/Loops/obj/Debug/net9.0/Loops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8cef07b512f766b1f450c836682e31fdf77019")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bdf460e207fd9919226a716f56ae3862b947d80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
